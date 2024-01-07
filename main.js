15cd09edbf025ea3051ef491848ce5f023eaff83
ANVARcik-pro <155723962+ANVARcik-pro@users.noreply.github.com>
2024-01-07 15:40:37 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,9 @@
-let btn = document.querySelector('.btn');
-let btn1 = document.querySelector('.btn1');
-let big = document.querySelector('.big');
-let box = document.querySelector('.box');
+let btn1 = document.querySelector('.btn1')
+let big1 = document.querySelector('.big1')
+
+btn1.addEventListener('click',function(){
+    big1.classList.toggle('new');
+    btn1.classList.toggle('new2');
+});
 
 
-
-btn.addEventListener('click', function(){
-    if(big.classList.contains('new1')){
-       big.classList.remove('new1')
-       big.textContent = 'salom';
-    } else {
-       big.classList.add('new1')
-       big.textContent ='xayr';
-    }
-    box.classList.add('new2')
-    btn1.classList.contain('new2')
-})
